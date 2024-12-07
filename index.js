64b856aeccbd897051295dc08b0cf81934c95f6a
Fayaz-shaik <fayazshaik1722@gmail.com>
2024-12-07 17:18:27 +0530

    Added functionality for Endpoint2 : Apply DIscount based on membership status

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 
 const { resolve } = require('path');
 
-const app = express();
+let app = express();
 const port = 3010;
 
-app.use(cors);
+app.use(cors());
 
 //Server Side Values
 let taxRate = 5
@@ -18,8 +18,13 @@ let loyaltyRate = 2;
 // });
 
 app.get("/cart-total",(req,res)=>{
-  res.send((parseFloat(res.query.newItemPrice)+parseFloat(res.query.cartTotal)).toString())
-})
+  res.send((parseFloat(req.query.newItemPrice)+parseFloat(req.query.cartTotal)).toString())
+});
+
+app.get("/membership-discount",(req,res)=>{
+  let cartTotal = parseFloat(req.query.cartTotal)
+  res.send(((req.query.isMember)?(cartTotal*(100-discountPercentage)/100):(cartTotal)).toString())
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
