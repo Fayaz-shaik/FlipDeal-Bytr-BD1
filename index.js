1b3c943cbd92f75752893e97615eac79020f9ebe
Fayaz-shaik <fayazshaik1722@gmail.com>
2024-12-07 17:10:08 +0530

    Add totalPrice functionality

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(cors);
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+//Server Side Values
+let taxRate = 5
+let discountPercentage = 10;
+let loyaltyRate = 2;
+
+// app.get('/', (req, res) => {
+//   res.sendFile(resolve(__dirname, 'pages/index.html'));
+// });
+
+app.get("/cart-total",(req,res)=>{
+  res.send((parseFloat(res.query.newItemPrice)+parseFloat(res.query.cartTotal)).toString())
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
